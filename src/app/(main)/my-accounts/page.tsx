c588a242fd84219aca35dbc98707e57af2b61e94
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-30 16:12:33 -0400

    addingi background img and logout button to my accounts page

--- src/app/(main)/my-accounts/page.tsx
+++ src/app/(main)/my-accounts/page.tsx
@@ -6,15 +6,17 @@ import styles from "@/styles/my-accounts.module.css"
 import { Account } from "@/types/Account"
 import { AccountRequest } from "@/types/AccountRequest"
 import { AccountType } from "@/types/AccountType"
+import { useRouter } from "next/navigation"
 import { useContext, useEffect, useState } from "react"
 
 export default function MyAccounts() {
     const [error, setError] = useState<String>("")
     const [loading, setLoading] = useState<boolean>(false)
-    const {token} = useContext(AuthContext)
+    const {token, logout} = useContext(AuthContext)
     const [accountRequest, setAccountRequest] = useState<AccountRequest>({name: "", type: "", balance: "0.00"})
     const [accounts, setAccounts] = useState<Account[]>([])
     const [refresh, setRefresh] = useState<number>(0)
+    const router = useRouter()
 
     useEffect(() => {
         setLoading(true)
@@ -81,47 +83,50 @@ export default function MyAccounts() {
     }
 
     return (
-        <div className="book">
-            <div className="page-left">
-                <h1 className={styles.title2}>Add a New Account</h1>
-                <div className={styles.formbar}>Please enter your account information below</div>
-                <form className={styles.form} onSubmit={handleSubmit}>
-                    <input type="text" name="name" placeholder="Account Name*" value={accountRequest.name} onChange={handleChange} disabled={loading} required></input>
-                    <select name="type" disabled={loading} onChange={handleChange}>
-                        <option value="">Account Type*</option>
-                        <option value={AccountType.CHECKING}>CHECKING</option>
-                        <option value={AccountType.SAVINGS}>SAVINGS</option>
-                        <option value={AccountType.CASH}>CASH</option>
-                        <option value={AccountType.OTHER}>OTHER</option>
-                    </select>
-                    <input type="text" name="balance" placeholder="0.00" value={accountRequest.balance} onChange={handleChange} disabled={loading} required></input>
-                    <button className="buttonSecondary" type="submit" disabled={loading}>Create</button>
-                    {error && <p>{error}</p>}
-                </form>
-            </div>
-            <div className="page-right">
-                <h1 className={styles.title}>My Accounts</h1>
-                <table className={styles.table}>
-                    <thead className={styles.thead}>
-                        <tr className={styles.tr}>
-                            <th className={styles.th}>Name</th>
-                            <th className={styles.th}>Type</th>
-                            <th className={styles.th}>Balance</th>
-                        </tr>
-                    </thead>
-                    <tbody className={styles.tbody}>
-                    {
-                        accounts.map(account => (
-                            <tr className={styles.tr} key={account.id}>
-                                <td className={styles.td}>{account.name}</td>
-                                <td className={styles.td}>{account.type}</td>
-                                <td className={styles.td}>${account.balance}</td>
+        <div className="background">
+            <div className="book">
+                <div className="page-left">
+                    <h1 className={styles.title2}>Add a New Account</h1>
+                    <div className={styles.formbar}>Please enter your account information below</div>
+                    <form className={styles.form} onSubmit={handleSubmit}>
+                        <input type="text" name="name" placeholder="Account Name*" value={accountRequest.name} onChange={handleChange} disabled={loading} required></input>
+                        <select name="type" disabled={loading} onChange={handleChange}>
+                            <option value="">Account Type*</option>
+                            <option value={AccountType.CHECKING}>CHECKING</option>
+                            <option value={AccountType.SAVINGS}>SAVINGS</option>
+                            <option value={AccountType.CASH}>CASH</option>
+                            <option value={AccountType.OTHER}>OTHER</option>
+                        </select>
+                        <input type="text" name="balance" placeholder="0.00" value={accountRequest.balance} onChange={handleChange} disabled={loading} required></input>
+                        <button className="buttonSecondary" type="submit" disabled={loading}>Create</button>
+                        {error && <p>{error}</p>}
+                    </form>
+                </div>
+                <div className="page-right">
+                    <h1 className={styles.title}>My Accounts</h1>
+                    <table className={styles.table}>
+                        <thead className={styles.thead}>
+                            <tr className={styles.tr}>
+                                <th className={styles.th}>Name</th>
+                                <th className={styles.th}>Type</th>
+                                <th className={styles.th}>Balance</th>
                             </tr>
-                        ))
-                    }
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className={styles.tbody}>
+                        {
+                            accounts.map(account => (
+                                <tr className={styles.tr} key={account.id}>
+                                    <td className={styles.td}>{account.name}</td>
+                                    <td className={styles.td}>{account.type}</td>
+                                    <td className={styles.td}>${account.balance}</td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </table>
+                </div>
             </div>
+            <button className="logout" onClick={logout}>logout</button>
         </div>
     )
 }
