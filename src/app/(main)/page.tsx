64f62a227f7e90c600ed382fd9d63a64f1c8e541
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-30 01:49:20 -0400

    minor UI changes, added background, login/signup input styling, fix header background and positioning, moved book position

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,6 +7,7 @@ import { useContext, useEffect } from "react"
 
 export default function BudgetBook() {
     const router = useRouter()
+    const {logout} = useContext(AuthContext)
     const {token, currentCustomer, loading} = useContext(AuthContext)
 
     //redirect to home if no local stored customer info
@@ -18,32 +19,35 @@ export default function BudgetBook() {
     if (loading || (!token || !currentCustomer)) {return null}
     
     return (
-        <div className={styles.cover} onClick={() => router.push('/table-of-contents')}>
-            <div className={styles.top}>
-                <div className="logo">
-                    <img className="logo_img" src="/logo.png"></img>
-                    <h1 className="logo_text">BudgetBook</h1>
+        <div className="background">
+            <div className={styles.cover} onClick={() => router.push('/table-of-contents')}>
+                <div className={styles.top}>
+                    <div className="logo">
+                        <img className="logo_img" src="/logo.png"></img>
+                        <h1 className="logo_text">BudgetBook</h1>
+                    </div>
+                    <div className={styles.dots}>
+                        <div className={styles.dot}></div>
+                        <div className={styles.dot}></div>
+                        <div className={styles.dot}></div>
+                    </div>
                 </div>
+                <div className={styles.title}>
+                    <span className={styles.text_budget}>BUDGET</span>
+                    <br></br>
+                    <span className={styles.text_book}>BOOK</span>
+                </div>
+                <p className={styles.text}>Welcome back. It’s time to turn the page on your finances and start a new chapter—organize your budget, grow your savings, and build your future.</p>
+                <div className={styles.rectangle}></div>
                 <div className={styles.dots}>
                     <div className={styles.dot}></div>
                     <div className={styles.dot}></div>
                     <div className={styles.dot}></div>
                 </div>
+                <a className={styles.site_link} href='/'>www.budgetbook.com</a>
+                <img className={styles.city} src="/city.png"></img>
             </div>
-            <div className={styles.title}>
-                <span className={styles.text_budget}>BUDGET</span>
-                <br></br>
-                <span className={styles.text_book}>BOOK</span>
-            </div>
-            <p className={styles.text}>Welcome back. It’s time to turn the page on your finances and start a new chapter—organize your budget, grow your savings, and build your future.</p>
-            <div className={styles.rectangle}></div>
-            <div className={styles.dots}>
-                <div className={styles.dot}></div>
-                <div className={styles.dot}></div>
-                <div className={styles.dot}></div>
-            </div>
-            <a className={styles.site_link} href='/'>www.budgetbook.com</a>
-            <img className={styles.city} src="/city.png"></img>
+            <button className="logout" onClick={logout}>logout</button>
         </div>
     )
 }
--- src/app/(main)/table-of-contents/page.tsx
+++ src/app/(main)/table-of-contents/page.tsx
@@ -9,17 +9,19 @@ export default function TableOfContents() {
     const router = useRouter()
     const {logout} = useContext(AuthContext)
     return (
-        <div className="book">
-            <div className={styles.backCover} onClick={() => router.push('/')}></div>
-            <div className="page-right">
-                <h1 className={styles.title}>Table of Contents</h1>
-                <ol className={styles.contents}>
-                    <li onClick={() => router.push('/my-accounts')}>Manage Accounts</li>
-                    <li onClick={() => router.push('/my-transactions')}>Manage Transactions</li>
-                    <li onClick={() => router.push('/my-budgets')}>Manage Budgets</li>
-                    <li onClick={() => router.push('/spending-summary')}>Spending Summary</li>
-                    <li onClick={() => router.push('/edit-profile')}>Edit Profile</li>
-                </ol>
+        <div className="background">
+            <div className="book">
+                <div className={styles.backCover} onClick={() => router.push('/')}></div>
+                <div className="page-right">
+                    <h1 className={styles.title}>Table of Contents</h1>
+                    <ol className={styles.contents}>
+                        <li onClick={() => router.push('/my-accounts')}>Manage Accounts</li>
+                        <li onClick={() => router.push('/my-transactions')}>Manage Transactions</li>
+                        <li onClick={() => router.push('/my-budgets')}>Manage Budgets</li>
+                        <li onClick={() => router.push('/spending-summary')}>Spending Summary</li>
+                        <li onClick={() => router.push('/edit-profile')}>Edit Profile</li>
+                    </ol>
+                </div>
             </div>
             <button className="logout" onClick={logout}>logout</button>
         </div>
