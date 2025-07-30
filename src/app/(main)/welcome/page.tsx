69e0a60e8cc3525e333f45aa07bf7d465a733076
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-30 14:57:02 -0400

    completed redesign for welcome page

--- src/app/(main)/welcome/page.tsx
+++ src/app/(main)/welcome/page.tsx
@@ -1,15 +1,30 @@
+'use client'
+
 import Header from "@/components/Header"
 import styles from "@/styles/welcome.module.css"
+import { useRouter } from "next/navigation"
 
 export default function Welcome() {
+  const router = useRouter()
+  
   return (
-    <div className={styles.home}>
-      <Header/>
-      <div className={styles.slide}>
-        <video className={styles.video} autoPlay muted>
-          <source src="intro_video.mp4" type="video/mp4"/>
-          <img src="/intro_video_fallback.png" alt="budget book displayed on a computer screen"/>
-        </video>
+    <div className={styles.welcome}>
+      <Header />
+      <div className={styles.backgroundLayout}>
+        <div className={styles.videoWrapper}>
+          <video className={styles.backgroundVideo} autoPlay muted playsInline>
+            <source src="intro_video.mp4" type="video/mp4" />
+            <img src="/intro_video_fallback.png" alt="fallback image" />
+          </video>
+          <div className={styles.videoFade}></div>
+        </div>
+        <div className={styles.content}>
+          <h1>Budget.</h1>
+          <h1>Track it.</h1>
+          <h1>Save.</h1>
+          <p>Take control of your financial future by tracking your daily spending habits, identifying opportunities to save, and building lasting financial security—all with the powerful tools and insights of Budget Book.</p>
+          <button className="buttonSecondary" onClick={() => router.push('/login')}>Get Started</button>
+        </div>
       </div>
     </div>
   )
