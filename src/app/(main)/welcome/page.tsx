862b8173f7e88d893898b6e50e05696791bcb3f6
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-30 14:02:31 -0400

    begin changing welcome page UI

--- src/app/(main)/welcome/page.tsx
+++ src/app/(main)/welcome/page.tsx
@@ -1,15 +1,11 @@
 import Header from "@/components/Header"
-import styles from "@/styles/home.module.css"
+import styles from "@/styles/welcome.module.css"
 
-export default function Home() {
+export default function Welcome() {
   return (
     <div className={styles.home}>
       <Header/>
       <div className={styles.slide}>
-        <div className={styles.block}>
-          <h1 className={styles.title}>Track Your Spending.<br/>Grow Your Savings.<br/>With Budget Book.</h1>
-          <p className={styles.body}>Budget Book helps you take control of your finances. Track your accounts, monitor spending, build custom budgets, categorize expenses, and set financial goals with ease.</p>
-        </div>
         <video className={styles.video} autoPlay muted>
           <source src="intro_video.mp4" type="video/mp4"/>
           <img src="/intro_video_fallback.png" alt="budget book displayed on a computer screen"/>
