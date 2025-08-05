74c5be50bd003500b51b2547a55900693d18e61e
Kayla George <kaylatrinitygeorge@gmail.com>
2025-08-05 10:18:07 -0400

    removing asteriks from fields for edit profile

--- src/app/(main)/edit-profile/page.tsx
+++ src/app/(main)/edit-profile/page.tsx
@@ -101,8 +101,8 @@ export default function EditProfile() {
                     <input type="text" name="firstName" placeholder="First Name*" value={updateProfileRequest.firstName} onChange={handleChange} disabled={loading} required></input>
                     <input type="text" name="lastName" placeholder="Last Name*" value={updateProfileRequest.lastName} onChange={handleChange} disabled={loading} required></input>
                     <input type="email" name="email" placeholder="Email*" value={updateProfileRequest.email} onChange={handleChange} disabled={loading} required></input>
-                    <input type="password" name="password" placeholder="Password*" value={updateProfileRequest.password} onChange={handleChange} disabled={loading} minLength={8}></input>
-                    <input type="password" placeholder="Confirm Password*" ref={confirmPassword} onChange={handleChange} disabled={loading}></input>
+                    <input type="password" name="password" placeholder="Password" value={updateProfileRequest.password} onChange={handleChange} disabled={loading} minLength={8}></input>
+                    <input type="password" placeholder="Confirm Password" ref={confirmPassword} onChange={handleChange} disabled={loading}></input>
                     <input type="tel" name="phoneNumber" placeholder="Phone Number" value={updateProfileRequest.phoneNumber} onChange={handleChange} disabled={loading}></input>
                     <button className="buttonPrimary" type="submit" disabled={loading}>Save</button>
                     {error && <p>{error}</p>}
